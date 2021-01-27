20d9d1ad12501065d2e41d0f6a6f2cbd81441f3d
BigOBasheer <838@holbertonschool.com>
2021-01-27 23:19:03 +0000

    Add Task 7

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -15,6 +15,28 @@ def get_states():
             list_states.append(state.to_dict())
             return jsonify(list_states)
 
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """ Retrieves the list of all State objects """
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_state(state_id):
+    """ Retrieves the list of all State objects """
+    state = storage.get(State, state_id)
+    if state:
+        state.delete()
+        storage.save()
+        return ({}, 200)
+    abort(404)
+
+
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def post_state():
     """ Retrieves the list of all State objects """
@@ -24,7 +46,19 @@ def post_state():
             abort(400, 'Not a JSON')
         list_states = State(**request.get_json())
         if 'name' not in list_states.to_dict().keys():
-                abort(400, 'Missing name'
+                abort(400, 'Missing name')
         list_states.save()
-            return (list_states.to_dict(), 201)
+        return (list_states.to_dict(), 201)
 
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def put_state(state_id):
+    """ Retrieves the list of all State objects """
+    state = storage.get(State, state_id)
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+    for k, v in request.get_json().items():
+        setattr(state, k, v)
+        storage.save()
+        return state.to_dict()
+    abort(404)
