d27f57b91ec0d69e51f67dbc4495f5dcc790fbe6
jashjchoi <jashjchoi@gmail.com>
2021-01-28 18:01:23 +0000

    Revert "Merge pull request #23 from jashjchoi/jaz"
    This reverts commit 55a4059a0444d23146c70e3bdcdb97687fe04721, reversing
    changes made to 933067216f303899ab565743abeb8871a9caa6d4.

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,10 +9,11 @@ from api.v1.views import app_views
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     """ Retrieves the list of all State objects """
-    list_states = []
-    for state in storage.all("State").values():
-        list_states.append(state.to_dict())
-    return jsonify(list_states)
+    if request.method == "GET":
+        list_states = []
+        for state in storage.all(State).values():
+            list_states.append(state.to_dict())
+        return make_response(jsonify(list_states), 200)
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
@@ -54,12 +55,10 @@ def post_state():
 def put_state(state_id):
     """ Retrieves the list of all State objects """
     state = storage.get(State, state_id)
-    if state is None:
-        abort(404)
     if not request.get_json():
-        return make_response("Not a JSON"), 400)
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
     for k, v in request.get_json().items():
-        if key not in ["id", "created_at", "updated_at"]:
-            setattr(state, k, v)
-    storage.save()
-    return make_response(jsonify(state.to_dict()), 200)
+        setattr(state, k, v)
+        storage.save()
+        return make_response(jsonify(state.to_dict()), 200)
+    abort(404)
