4cb6e0f8dc1d58a548503447826980e514779e11
BigOBasheer <838@holbertonschool.com>
2021-01-28 22:50:12 +0000

    Add Task 7

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,11 +9,10 @@ from api.v1.views import app_views
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     """ Retrieves the list of all State objects """
-    if request.method == "GET":
-        list_states = []
-        for state in storage.all(State).values():
-            list_states.append(state.to_dict())
-        return make_response(jsonify(list_states), 200)
+    list_states = []
+    for state in storage.all(State).values():
+        list_states.append(state.to_dict())
+    return make_response(jsonify(list_states), 200)
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
@@ -28,7 +27,7 @@ def get_state(state_id):
 @app_views.route('/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_state(state_id):
-    """Delete a State by state_id"""
+    """ Delete a State by state_id """
     state_id = storage.get(State, state_id)
     if state_id is not None:
         storage.delete(state_id)
@@ -39,16 +38,15 @@ def delete_state(state_id):
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def post_state():
-    """Create a new State"""
-    if request.method == 'POST':
-        list_states = []
-        if not request.is_json:
-            return make_response("Not a JSON", 400)
-        list_states = State(**request.get_json())
-        if 'name' not in list_states.to_dict().keys():
-            return make_response("Missing name", 400)
-        list_states.save()
-        return make_response(jsonify(list_states.to_dict()), 201)
+    """ Create a new State """
+    list_states = []
+    if not request.is_json:
+        return make_response("Not a JSON", 400)
+    list_states = State(**request.get_json())
+    if 'name' not in list_states.to_dict().keys():
+        return make_response("Missing name", 400)
+    list_states.save()
+    return make_response(jsonify(list_states.to_dict()), 201)
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
